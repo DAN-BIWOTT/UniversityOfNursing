b922185ab3c387a2ddc25312b2729eba3e961e81
SHAWN <dankibiwott254@gmail.com>
2021-12-22 23:59:05 -0800

    initialized state

--- src/pages/adminPages/OrderDetail.js
+++ src/pages/adminPages/OrderDetail.js
@@ -1,18 +1,17 @@
-import React, { useState } from 'react'
+import React, { useState, initialState } from 'react'
 import styled from 'styled-components'
 import DetailMain from '../../components/adminPages/DetailMain'
 import Nav from '../../components/main/Nav'
 import Sidebar from '../../components/sidebar/sidebar'
 
 const OrderDetail = ({location}) => {
-    const [ItemOrderId, setItemOrderId] = useState(initialState)
-    setItemOrderId(location.state.orderId)
+    const [itemOrderId, setItemOrderId] = useState(location.state.orderId)
+    
     return (
         <Container>
             <Sidebar/>
             <Nav/>
-            {/* <DetailMain orderId={location.state.orderId}/> */}
-            <DetailMain orderId={ItemOrderId}/>
+            <DetailMain orderId={itemOrderId}/>
         </Container>
     )
 }
