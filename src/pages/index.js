fbf971f7981fd97b174456a85e5c4acf457efd1f
SHAWN <dankibiwott254@gmail.com>
2021-12-28 12:03:54 +0300

    tawk to

--- src/pages/index.js
+++ src/pages/index.js
@@ -22,6 +22,17 @@ const tawkToKey = '4dd464c8c21ae08a883035ce9eb7eb9baad96c68'
 useEffect(() => {
     tawkTo(tawkToPropertyId, tawkToKey)
 }, [])
+
+
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/61caba6fc82c976b71c3c3c1/1fnvughtt';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
   return (
     <Layout>
       <Seo
