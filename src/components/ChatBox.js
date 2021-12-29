905fc634f5d8804668458d2863abe2a03e52ee99
SHAWN <dankibiwott254@gmail.com>
2021-12-29 15:51:59 +0300

    project files

--- src/components/ChatBox.js
+++ src/components/ChatBox.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import styled from "styled-components";
 import ChatBody from "./chatPartials/ChatBody";
-import { BiChat } from "react-icons/Bi/index";
+import { BiChat } from "react-icons/bi";
 import { CSSTransition } from 'react-transition-group';
 import "./chatPartials/chatBox.css";
 
