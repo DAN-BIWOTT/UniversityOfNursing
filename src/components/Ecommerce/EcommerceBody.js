b048fa14c376db6946a8e8c88505991bf0ca520e
SHAWN <dankibiwott254@gmail.com>
2022-01-23 00:42:46 +0300

    files

--- src/components/Ecommerce/EcommerceBody.js
+++ src/components/Ecommerce/EcommerceBody.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React,{useState,useEffect} from "react";
 import styled from "styled-components";
 import { AllProducts_query } from "../../graphQl/uonQueries";
 import SingleProductBody from "./SingleProductBody";
