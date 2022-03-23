236c62b3a21fd777479ea3cae276e3c693f330cc
JODAWAKI <jodawaki.enterprise@gmail.com>
2022-03-23 16:32:06 +0300

    cards to list

--- src/components/cards/service.js
+++ src/components/cards/service.js
@@ -16,22 +16,5 @@ const styles = {
     backgroundColor:"#fff",
     float:'left',
 margin:"0 5px",
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'center',
-    minHeight: [107, null, null, 130],
-    textAlign: 'center',
-    p: {
-      fontWeight: 600,
-      fontSize: [2, null, null, '17px'],
-      lineHeight: 1.77,
-      color: '#968F89',
-    },
-    span: {
-      color: 'primary',
-      fontWeight: 700,
-      fontSize: [1, null, null, '15px'],
-      lineHeight: 2,
-    },
   },
 };
