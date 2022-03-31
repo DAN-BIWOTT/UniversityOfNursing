94cdad2c5d9617aeaf6c7f0f48f8aa466269ea1d
JODAWAKI <jodawaki.enterprise@gmail.com>
2022-04-01 02:41:44 +0300

    added a new table componenet

--- src/components/main/users/UserDetailTable.js
+++ src/components/main/users/UserDetailTable.js
@@ -2,6 +2,7 @@ import React from 'react'
 import styled from 'styled-components';
 
 const UserDetailTable = ({tableData}) => {
+    console.log(tableData)
   return (
     <div>
         <Title>{tableData.client[0]?.full_name}</Title>
