aa1b4dcc60a58388ecb752eaba98b4fa991cee83
JODAWAKI <jodawaki.enterprise@gmail.com>
2022-06-14 15:17:01 +0300

    foil

--- src/components/auth/SignIn.js
+++ src/components/auth/SignIn.js
@@ -24,6 +24,7 @@ const SignIn = () => {
     event.preventDefault(event);
     setPageLoader(true)
     setWaitingButton("wait");
+    
     if (email !== "") {
       const response = await fetch(
         "https://elegant-phoenix-17.hasura.app/v1/graphql",
